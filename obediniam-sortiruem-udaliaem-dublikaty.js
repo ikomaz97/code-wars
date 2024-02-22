dfd8ad679040971fffe47dd93b4193daf492ec64
ikomaz97 <ikomaz97@gmail.com>
2024-02-22 20:05:50 +0400

    07-22-24

--- obediniam-sortiruem-udaliaem-dublikaty.js
+++ obediniam-sortiruem-udaliaem-dublikaty.js
@@ -7,3 +7,10 @@ function mergeArrays(arr1, arr2) {
 
     return mergedArray;
 }
+
+const warnTheSheep = (queue) => {
+    const wolfIndex = queue.indexOf("wolf");
+    return wolfIndex === queue.length - 1 ?
+        "Pls go away and stop eating my sheep" :
+        `Oi! Sheep number ${queue.length - wolfIndex - 1}! You are about to be eaten by a wolf!`;
+}
