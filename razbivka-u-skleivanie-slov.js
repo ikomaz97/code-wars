d6a4f8a3888560ecb4be9ae90912709c0a482eff
ikomaz97 <ikomaz97@gmail.com>
2024-02-22 11:49:30 +0400

    05-22-24

--- razbivka-u-skleivanie-slov.js
+++ razbivka-u-skleivanie-slov.js
@@ -1,3 +1,7 @@
 const shortcut = (string) => {
     return string.split("").filter(el => !['a', 'e', 'i', 'o', 'u'].includes(el)).join("");
 }
+
+function shortcut(string){
+    return string.replace(/[aeiou]/g,'')
+}
