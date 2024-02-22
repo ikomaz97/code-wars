10e63d339e92a63b61a1f508133e904cb1cdbcb8
ikomaz97 <ikomaz97@gmail.com>
2024-02-22 11:36:23 +0400

    03-22-24

--- schetchik-plohih-slov.js
+++ schetchik-plohih-slov.js
@@ -15,3 +15,10 @@ function well(x) {
         return 'I smell a series!';
     }
 }
+
+
+const well = x => {
+    const good_count = x.filter(x => x == 'good').length;
+    return good_count < 1 ? 'Fail!' :
+        good_count < 3 ? 'Publish!' : 'I smell a series!';
+}
