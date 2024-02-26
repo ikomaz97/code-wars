e5778995268abbb7abbef39710dee9d4a72b40ea
ikomaz97 <ikomaz97@gmail.com>
2024-02-26 12:25:42 +0400

    01-26-24

--- 04.js
+++ 04.js
@@ -1,3 +1,16 @@
 function problem(x){
     return typeof x !== 'number' ? "Error" : x * 50 + 6;
 }
+
+
+function peopleWithAgeDrink(age) {
+    if (age < 14) {
+        return "drink toddy";
+    } else if (age < 18) {
+        return "drink coke";
+    } else if (age < 21) {
+        return "drink beer";
+    } else {
+        return "drink whisky";
+    }
+}
