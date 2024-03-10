360971f42632ad7077de9a3786a932d59f3056a5
ikomaz97 <ikomaz97@gmail.com>
2024-03-10 12:09:30 +0400

    03-10-03

--- sortirovka-u-dobalenie-numbers.js
+++ sortirovka-u-dobalenie-numbers.js
@@ -63,6 +63,25 @@ function chromosomeCheck(sperm) {
 const main = (verb, noun) => verb + noun;
 
 
+function getLength(arr){
+    return arr.length;
+}
+function getFirst(arr){
+    return arr[0];
+}
+function getLast(arr){
+    return arr[arr.length-1];
+}
+function pushElement(arr){
+    arr.push(1);
+    return arr;
+}
+function popElement(arr){
+    arr.pop()
+    return arr;
+}
+
+
 
 
 
