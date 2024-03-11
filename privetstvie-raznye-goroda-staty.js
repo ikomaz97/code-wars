8bac9870f8ed7df632117974219cf916f75d6c29
ikomaz97 <ikomaz97@gmail.com>
2024-03-11 17:03:26 +0400

    01-11-03

--- privetstvie-raznye-goroda-staty.js
+++ privetstvie-raznye-goroda-staty.js
@@ -2,3 +2,12 @@ function sayHello(name, city, state) {
     const fullName = name.join(' '); // Объединяем элементы массива имени в одну строку с пробелами между ними
     return `Hello, ${fullName}! Welcome to ${city}, ${state}!`;
 }
+
+const rainAmount = (mm) =>{
+    if (rainAmount === 40) {
+        return "You need to give your plant " + {rainAmount: 40} + " mm of water"
+    }
+    else   {
+        return "Your plant has had more than enough water for today!"
+    };
+}
