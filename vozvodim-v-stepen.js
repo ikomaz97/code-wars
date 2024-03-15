74304d050768dbfb2aa40564fdf0207c4ed4bc58
ikomaz97 <ikomaz97@gmail.com>
2024-03-15 11:46:28 +0400

    4-15-03

--- vozvodim-v-stepen.js
+++ vozvodim-v-stepen.js
@@ -1,3 +1,10 @@
 function apple(x){
     return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.' ;
 }
+
+const   getSize = (width, height, depth) => {
+    let area = 2 * (width * height + height * depth + width * depth);
+    let volume = width * height * depth;
+    let arr =[area, volume];
+    return arr;
+}
