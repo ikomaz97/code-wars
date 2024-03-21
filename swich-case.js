7e5bc8c9b8fe6a41592a02aad9ab35b810a28336
ikomaz97 <ikomaz97@gmail.com>
2024-03-21 15:17:29 +0400

    1-21-03

--- swich-case.js
+++ swich-case.js
@@ -11,3 +11,19 @@ function getDrinkByProfession(param) {
         default: return "Beer";
     }
 }
+
+
+function whatday(num) {
+
+    switch (num)  {
+        case 1 : return "Sunday";
+        case 2 : return "Monday";
+        case 3 : return "Tuesday";
+        case 4 : return "Wednesday";
+        case 5 : return "Thursday";
+        case 6 : return "Friday";
+        case 7 : return "Saturday";
+        default: return "Wrong, please enter a number between 1 and 7";
+
+    }
+}
