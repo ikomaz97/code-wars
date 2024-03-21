f86d4fd38c8edf6eb6e23a5c7a2d1ac627a65a85
ikomaz97 <ikomaz97@gmail.com>
2024-03-21 15:25:07 +0400

    3-21-03

--- swich-case.js
+++ swich-case.js
@@ -31,3 +31,11 @@ function whatday(num) {
 function whatday(num) {
     return ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][num-1]||'Wrong, please enter a number between 1 and 7'
 }
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+Dad = _ => { return d1+a2+d2; }
+Bee = _ => { return b1+e2+e2; }
+banana = _ => { return b2+a2+n2+a2+n2+a2; }
+answer1 = _ => { return "no"; }
+answer2 = _ => { return "no"; }
+answer3 = _ => { return "yes"; }
