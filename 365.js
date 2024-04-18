cfa3f87984a61b6730bf407ba9b0ad1e016c1c80
ikomaz97 <ikomaz97@gmail.com>
2024-04-18 14:02:27 +0400

    3-17-044

--- 365.js
+++ 365.js
@@ -13,19 +13,5 @@ type ChangeUserPasswordTypeAT = {
     }
 }
 
-export const userReducer =
-    (state: UserType[], action: ChangeUserPasswordTypeAT): UserType[] => {
-    switch (action.type) {
-        case "CHANGE-USER-PASSWORD":
-            return state.map(u =>
-                u.id === action.payload.id
-                    ? {...u, password: action.payload.newPassword}
-                    : u)
-        default:
-            return state
-    }
 }
 
-[userID.toString()] используется для обращения к адресу пользователя в объекте addresses по его userID в виде строки.
-...addresses[userID.toString()] создает копию адреса пользователя, чтобы не изменять исходный объект напрямую.
-    [key]: newValue обновляет указанное поле key в адресе пользователя на новое значение newValue.
